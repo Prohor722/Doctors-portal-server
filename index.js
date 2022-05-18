67ec1131b7093b56930bbcfa836f40e005e69976
Prohor722 <prohor.b722@gmail.com>
2022-05-18 18:47:30 +0600

    users api token restricted

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ async function run(){
             res.send(result);
         })
 
-        app.get('/user', async(req,res)=>{
+        app.get('/user',verifyJWT, async(req,res)=>{
             const users = await userCollection.find().toArray();
 
             res.send(users);
